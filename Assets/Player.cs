41458407a1aebe20402bd10bfe3b9e8c66ed97b6
Wingszero <Wingszero@hntvlan569.0444.wlan.wireless-resnet.upenn.edu>
2015-02-16 18:19:26 -0500

    Add new attributes to Player

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -2,6 +2,15 @@
 using System.Collections;
 
 public class Player : MonoBehaviour {
+	private int max_hp; //health point
+	private int cur_hp;
+
+	private int max_level; //level
+	private int cur_level;
+
+	private int max_mp; //magic point
+	private int cur_mp;
+
 
 	// Use this for initialization
 	void Start () {
